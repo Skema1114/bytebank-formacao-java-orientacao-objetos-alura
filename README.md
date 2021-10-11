9668d296f8870223bf479947ad2f0bd97414a280
Rafael Aranda Martins <skema1114@hotmail.com>
2021-10-11 18:59:33 -0300

    Iniciando o curso e terminando a parte 02 do mesmo

---
+++ README.md
@@ -0,0 +1,13 @@
+# BytebankJavaOrientacaoObjetosAlura
+
+## Neste projeto:
+
+- Java OO: Introdução à Orientação a Objetos:
+
+    - https://cursos.alura.com.br/course/java-introducao-orientacao-objetos
+
+## Comandos utilizados
+
+## Anotações
+
+## Links
---
+++ src/CriarConta.java
@@ -0,0 +1,35 @@
+public class CriarConta {
+    public static void main(String[] args) {
+
+        Conta primeiraConta = new Conta();
+        primeiraConta.saldo = 200;
+        System.out.println(primeiraConta.saldo);
+
+        primeiraConta.saldo += 100;
+        System.out.println(primeiraConta.saldo);
+
+        Conta segundaConta = new Conta();
+        segundaConta.saldo = 50;
+
+        System.out.println("Primeira conta saldo: " + primeiraConta.saldo);
+        System.out.println("Segunda conta saldo: " + segundaConta.saldo);
+
+        System.out.println("Primeira conta GERAL: saldo = " + primeiraConta.saldo +
+                " | titular = " + primeiraConta.titular +
+                " | agência = " + primeiraConta.agencia +
+                " | número = " + primeiraConta.numero);
+        System.out.println("Segunda conta GERAL: saldo = " + segundaConta.saldo +
+                " | titular = " + segundaConta.titular +
+                " | agência = " + segundaConta.agencia +
+                " | número = " + segundaConta.numero);
+
+        if (primeiraConta == segundaConta) {
+            System.out.println("São a mesmíssima conta!");
+        } else {
+            System.out.println("São contas diferentissímas!");
+        }
+
+        System.out.println("Referencia da primeira conta " + primeiraConta +
+                " e referencia da segunda conta " + segundaConta);
+    }
+}
